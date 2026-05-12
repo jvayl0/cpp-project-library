55996acbf24cd2d273a6ebfd25a823530d600e8c
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-12 18:06:41 +0300

    small change

--- HEAD/User.hpp
+++ HEAD/User.hpp
@@ -5,21 +5,10 @@ protected:
     char* username;
     char* password;
 
-    void copyFrom(const User& other);
-    void free();
-
 public:
     User(const char* username, const char* password);
-    User(const User& other);
-    User(User&& other) noexcept;
     virtual ~User();
 
-    const char* getUsername() const;
-    const char* getPassword() const;
-
-    void setUsername(const char* username);
-    void setPassword(const char* password);
-
     virtual bool isAdmin() const = 0;
-    virtual User* clone() const = 0;
+    const char* getUsername() const;
 };
