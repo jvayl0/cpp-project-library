ea2a2cab1bc350b2d0321337d2698e43d63afbaf
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-12 14:56:30 +0300

    adding free() method

--- source/objects/Book.cpp
+++ source/objects/Book.cpp
@@ -12,7 +12,6 @@ namespace {
 }
 
 // PRIVATE METHODS
-
 void Book::swap(Book& other) noexcept {
     mySwap(id, other.id);
     mySwap(author, other.author);
@@ -23,4 +22,23 @@ void Book::swap(Book& other) noexcept {
     mySwap(rating, other.rating);
     mySwap(keywords, other.keywords);
     mySwap(keywordsCount, other.keywordsCount);
+}
+
+void Book::free(){
+    delete[] author;
+    delete[] title;
+    delete[] genre;
+    delete[] description;
+    
+    for(size_t i = 0; i < keywordsCount; i++){
+        delete[] keywords[i];
+    }
+    delete[] keywords;
+
+    author = nullptr;
+    title = nullptr;
+    genre = nullptr;
+    description = nullptr;
+    keywords = nullptr;
+    keywordsCount = 0;
 }
