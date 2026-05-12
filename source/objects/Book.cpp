2310b1c85a4085914ddc2261f725f9a629fd8ff6
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-12 14:52:46 +0300

    adding mySwap private method

--- source/objects/Book.cpp
+++ source/objects/Book.cpp
@@ -11,3 +11,16 @@ namespace {
     }
 }
 
+// PRIVATE METHODS
+
+void Book::swap(Book& other) noexcept {
+    mySwap(id, other.id);
+    mySwap(author, other.author);
+    mySwap(title, other.title);
+    mySwap(genre, other.genre);
+    mySwap(description, other.description);
+    mySwap(year, other.year);
+    mySwap(rating, other.rating);
+    mySwap(keywords, other.keywords);
+    mySwap(keywordsCount, other.keywordsCount);
+}
