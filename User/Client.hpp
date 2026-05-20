e0108271df57577fd7fac6b7683f4ae999c66abb
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-20 10:26:19 +0300

    client hpp

--- User/Client.hpp
+++ User/Client.hpp
@@ -1,9 +1,10 @@
 #pragma once
-
 #include "User.hpp"
 
 class Client : public User {
 public:
     Client(const char* username, const char* password);
+
     bool isAdmin() const override;
+    User* clone() const override;
 };
