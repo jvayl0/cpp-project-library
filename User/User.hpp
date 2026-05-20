812496a1ebabff65d1f81d1f712f281de638518c
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-20 10:20:19 +0300

    updating userhpp

--- User/User.hpp
+++ User/User.hpp
@@ -5,11 +5,18 @@ protected:
     char* username;
     char* password;
 
+    void copyFrom(const User& other);
+    void free();
+
 public:
     User(const char* username, const char* password);
+    User(const User& other);
+    User& operator=(const User& other);
     virtual ~User();
 
-    virtual bool isAdmin() const = 0;
     const char* getUsername() const;
-    bool checkPassword(const char* pass) const;
+    bool checkPassword(const char* password) const;
+
+    virtual bool isAdmin() const = 0;
+    virtual User* clone() const = 0;
 };
