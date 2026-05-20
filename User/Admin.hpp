05aa30a91d88a5e2abcf2e28d10afb119637a180
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-20 10:24:32 +0300

    admin tweak

--- User/Admin.hpp
+++ User/Admin.hpp
@@ -1,9 +1,10 @@
 #pragma once
-
 #include "User.hpp"
 
 class Admin : public User {
 public:
     Admin(const char* username, const char* password);
+    
     bool isAdmin() const override;
+    User* clone() const override;
 };
