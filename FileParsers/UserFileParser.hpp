ab91d1429cba9b14033b91903d74b8e09a18b3cd
Ivaylo Popov <ivaylopopov@Ivaylos-MacBook-Air.local>
2026-05-21 15:00:44 +0300

    user file parser hpp

---
+++ FileParsers/UserFileParser.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "UserCollection.hpp"
+
+class UserFileParser {
+public:
+    static bool loadFromFile(const char* file, UserCollection& users);
+    static bool saveToFile(const char* file, UserCollection& users);
+};
